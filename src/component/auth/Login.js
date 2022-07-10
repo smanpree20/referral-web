657ba5d8c35536eacd2181bcb81d2751df4aec1d
smanpree20 <smanpree20@gmail.com>
2022-07-10 13:02:58 +0530

    do some updates

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -1,64 +1,58 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+ import pic from '../../asset/images/login_logo.png'
 import '../../asset/css/style.css';
 const Login = () => {
     return (
 
-        <section class="h-100 gradient-form" style={{backgroundColor: "#eee"}}>
-        <div class="container py-5 h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-xl-10">
-              <div class="card rounded-3 text-black">
-                <div class="row g-0">
-                  <div class="col-lg-6">
-                    <div class="card-body p-md-5 mx-md-4">
+<section class="h-100 gradient-form" style={{backgroundColor: "none"}}>
       
-                      <div class="text-center">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                          style={{width: "185px"}} alt="logo" />
-                        <h4 class="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+      <div class="container py-5 h-100" >
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-xl-10">
+            <div class="card rounded-3 text-black" >
+              <div class="row g-0">
+                <div class="col-lg-6">
+                  <div class="card-body p-md-5 mx-md-4">
+      
+                    <div class="text-center mb-5">
+                      <img src={pic}
+                        style={{width: "50px"}} alt="logo" />
+                    </div>
+      
+                    <form>
+                  
+      
+                      <div class="form-outline mb-4">
+                      <input type="email"  class="form-rounded" placeholder='Email'/>
                       </div>
       
-                      <form>
-                    
+                      <div class="form-outline mb-4">
+                      <input type="password" class="form-rounded" placeholder='Password'/>
       
-                        <div class="form-outline mb-4">
-                        <input type="email"  class="form-rounded" placeholder='Email'/>
-                        </div>
+                      </div>
+                      <div class="d-flex align-items-center justify-content-center pb-4">
+                        <p class="mb-0 me-2">Sign Up Now</p>
+                      </div>
       
-                        <div class="form-outline mb-4">
-                        <input type="password" class="form-rounded" placeholder='Password'/>
-
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center pb-4">
-                          <p class="mb-0 me-2">Sign Up Now</p>
-                        </div>
-      
-                        <div class="text-center pt-1 mb-5 pb-1">
-                          <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                            in</button>
-                          
-                        </div>
-                        <div class="text-center pt-1 mb-5 pb-1"><a class="text-muted" href="#!">Forgot password?</a></div>
+                      <div class="text-center pt-1 mb-5 pb-1">
+                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
+                          in</button>
                         
-                       
-                      </form>
+                      </div>
+                      <div class="text-center pt-1 mb-5 pb-1"><a class="text-muted" href="#!">Forgot password?</a></div>
+                      
+                     
+                    </form>
       
-                    </div>
-                  </div>
-                  <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                      <h4 class="mb-4">We are more than just a company</h4>
-                      <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
                   </div>
                 </div>
+              
               </div>
             </div>
           </div>
         </div>
+      </div>
       </section>
       
     )
